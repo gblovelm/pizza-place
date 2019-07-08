0cfed73fd9ca79c19af5ba3d3b3a82c43c3a4345
Martin Lovell <gblovelm@gmail.com>
2019-07-08 10:42:30 +0100

    first commit

---
+++ server-one.js
@@ -0,0 +1,12 @@
+// This is a simple server that works directly with the HTTP module.
+const http = require("http");
+
+
+const server = http.createServer(function(req, res) {
+  console.log(`user visited ${req.url}`);
+  res.end("hello!");
+});
+
+
+console.log("listening on http://localhost:3000");
+server.listen(3000);
